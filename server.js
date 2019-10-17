2b3e16fdb7e36ac655e26bc3e5b43c0a86c0410b
Silvestre <emmayuel80@gmail.com>
2019-10-17 16:31:18 -0500

    arreglado el error

--- server.js
+++ server.js
@@ -3,13 +3,3 @@ const app = express()
 
 app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'))
 app.listen(3000, () => console.log('Ejecutando en el puerto 3000'))
-
-const value = 2
-
-function test () {
-  switch (value) {
-    case 1:
-      console.log(1)
-      break
-  }
-}
