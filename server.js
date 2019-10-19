f0fe295936798b6968010d58b8ac52ed7c7d97e8
Silvestre <emmayuel80@gmail.com>
2019-10-18 22:09:48 -0500

    Agregada regla de requerimiento del ";"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'))
-app.listen(port, () => console.log('Ejecutando en el puerto 3000.'))
+app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'));
+app.listen(port, () => console.log('Ejecutando en el puerto 3000.'));
