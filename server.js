664a6e7f77418a7c780db564544f46f6ff9a05ee
Silvestre <emmayuel80@gmail.com>
2019-10-20 15:23:38 -0500

    modificada estructura del servidor

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'));
-app.listen(port, () => console.log('Ejecutando en el puerto 3000..'));
+
+// Routes
+require('./app/routes.js')(app);
+
+io.on('connection', function (socket) {
+  //  Calculate service
+  require('./services/calculate.js')(io, socket);
+});
+
+http.listen(port, () => console.log('Ejecutando en el puerto 3000..'));
