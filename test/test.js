c200e737741fa9189f8a1be19362329952329e4c
Silvestre <emmayuel80@gmail.com>
2019-10-20 15:36:35 -0500

    agregado test unitario del servicio de operaciones

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 //  Test para probar mocha framework
 const assert = require('assert');
 const div = require('../services/division');
-
+const handleOperation = require('../services/handleOperation');
 describe('divider tests ', function () {
   var _numberA;
   var _numberB;
@@ -21,5 +21,26 @@ describe('divider tests ', function () {
     var result = div(_numberA, _numberB);
 
     assert.strictEqual(result, 0);
+  });
+});
+
+describe('Operation handler test ', function () {
+  var _numberA;
+  var _numberB;
+
+  it('should return the div of 2 numbers', function () {
+    _numberA = 6;
+    _numberB = 2;
+    var result = handleOperation(_numberA, _numberB, '4');
+
+    assert.strictEqual(result, 3);
+  });
+
+  it('should return undefined for operation equals 5', function () {
+    _numberA = 2;
+    _numberB = 0;
+    var result = handleOperation(_numberA, _numberB, '5');
+
+    assert.strictEqual(result, undefined);
   });
 });
