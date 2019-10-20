919959c72b3b6bfa254fa1be6112b64c6c1f79cc
Silvestre <emmayuel80@gmail.com>
2019-10-20 01:05:23 -0500

    Agregando front end de la calculadora

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'));
 app.listen(port, () => console.log('Ejecutando en el puerto 3000.'));
