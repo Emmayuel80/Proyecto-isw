73721649edcc75f92cc9a3c5a6ce0030a1507b45
Silvestre <emmayuel80@gmail.com>
2019-10-20 15:22:55 -0500

    Agregadas las rutas

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,12 @@
-const handleOperation = require('../services/handleOperation');
 module.exports = function (app) {
   // =====================================
   // HOME PAGE============================
   // =====================================
   app.get('/', function (req, res) {
-    res.render('index.ejs');
+    res.render('../public/views/index.ejs');
   });
 
-  app.post('/', function (req, res) {
-    const numA = req.body.numA;
-    const numB = req.body.numB;
-    const operation = req.body.operation;
-    const result = handleOperation(numA, numB, operation);
-    console.log(result);
+  app.get('*', (req, res) => {
+    res.render('not_fount.ejs');
   });
 };
