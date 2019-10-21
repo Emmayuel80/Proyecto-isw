f8b9abb0704c42d6ac9416f5a95ad80f8e8c4450
Emmayuel80 <emmayuel80@gmail.com>
2019-10-21 16:15:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('<h1>k onda profe</h1>'));
 app.listen(port, () => console.log('Ejecutando en el puerto 3000.'));
+
+function funcion () {
+  var par = 1;
+  switch (par) {
+    case 1: return 1;
+    case 2: return 2;
+    case 3: return 3;
+    case 4: return 4;
+    case 5: return 5;
+  }
+}
+
+console.log(funcion());
