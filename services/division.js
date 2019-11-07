5cdee5b8608a029b966ed91c02ed6709817aa536
Silvestre <emmayuel80@gmail.com>
2019-11-07 14:00:31 -0600

    eliminados archivos del ejemplo

--- services/division.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = function (A, B) {
-  if (B !== 0) {
-    return A / B;
-  } else {
-    return 0;
-  }
-};
--- services/multiplicacion.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (A, B) {
-  return A * B;
-};
