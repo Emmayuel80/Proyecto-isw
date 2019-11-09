d083ca7fc638b4264b03bfbd899d050578e70c42
Silvestre <emmayuel80@gmail.com>
2019-11-09 15:37:02 -0600

    implementacion del login

--- app/routes.js
+++ app/routes.js
@@ -4,21 +4,22 @@ module.exports = function (app, passport) {
   // HOME PAGE (with login links) ========
   // =====================================
   app.get('/', function (req, res) {
-    res.render('index.ejs'); // load the index.ejs file
+    res.render('../public/views/index.ejs'); // load the index.ejs file
   });
 
   // =====================================
   // LOGIN ===============================
   // =====================================
-
+  app.get('/login', function (req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('../public/views/login.ejs', { message: req.flash('loginMessage') });
+  });
   // process the login form
   app.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/login', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
-  // process the login form
-  // app.post('/login', do all our passport stuff here);
 
   // =====================================
   // SIGNUP ==============================
@@ -35,9 +36,14 @@ module.exports = function (app, passport) {
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/profile', isLoggedIn, function (req, res) {
-    res.render('profile.ejs', {
+    res.render('../public/views/profile.ejs', {
       user: req.user // get the user out of session and pass to template
     });
+  });
+
+  app.get('/profile/crearAsunto', function (req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('../public/views/crearAsunto.ejs', { sub: require('./getSubordinados')(req.user), user: req.user });
   });
 
   // =====================================
