4652ef8e21552fd5806a5bdc5ab98bdc301739fe
Silvestre <emmayuel80@gmail.com>
2019-11-10 18:29:41 -0600

    Agregada vista de asuntos

--- app/getAsuntos.js
+++ app/getAsuntos.js
@@ -1,7 +1,7 @@
 const connection = require('../config/database');
 
-module.exports = function () {
-  connection.query("select actividad,descripcion from asunto", function (_err, _rows) {
-    return _rows;
+module.exports = function (req, callback) {
+  connection.query('select Actividad, Descripcion from asunto where RFCS= "' + req.RFC + '"', function (_err, _rows) {
+    return callback(_rows);
   });
 };
