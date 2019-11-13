7e0148faeb3b691d9b1f0b9cba1c2e4695f7af82
Silvestre <emmayuel80@gmail.com>
2019-11-13 14:44:31 -0600

    agregados test unitarios

--- test/test.js
+++ test/test.js
@@ -1,3 +1,54 @@
 /* eslint-disable no-undef */
 //  Test para probar mocha framework
-//  const assert = require('assert');
+const assert = require('assert');
+const createAsunto = require('../services/createAsunto');
+const db = require('../config/database');
+const pool = db.getPool();
+const getAsuntos = require('../app/getAsuntos');
+const getSubordinados = require('../app/getSubordinados');
+
+describe('Tests de creacion de asunto', () => {
+  it('Debe crear un asunto con el titulo Test y descripcion Test unitario con 0 dias de termino', () => {
+    createAsunto({
+      body: {
+        Actividad: 'Test',
+        Descripcion: 'Test unitario',
+        DiasTermino: 0,
+        RFCS: 'null'
+      }
+    });
+    pool.getConnection(function (_err, connection) {
+      connection.query('select Actividad from asunto where Actividad = "Test"', function (_err, _rows) {
+        assert.strictEqual(_rows[0], 'Test');
+      });
+    });
+  });
+});
+
+describe('Test de obtencion de asuntos', () => {
+  it('Debe obtener los asuntos del subordinado de ejemplo', () => {
+    getAsuntos({
+      RFC: 'SUBOR1'
+    }, (result) => {
+      pool.getConnection((_err, connection) => {
+        connection.query('select Actividad from asunto where RFCS= "SUBOR1"', (_err, _rows) => {
+          assert.deepStrictEqual(result, _rows);
+        });
+      });
+    });
+  });
+});
+
+describe('Test de obtencion de subordinados', () => {
+  it('Debe obtener los subordinados del encargado de ejemplo', () => {
+    getSubordinados({
+      RFC: 'FDSK45385'
+    }, (result) => {
+      pool.getConnection((_err, connection) => {
+        connection.query("select * from subordinado where RFCE = 'FDSK45385'", (_err, _rows) => {
+          assert.deepStrictEqual(result, _rows);
+        });
+      });
+    });
+  });
+});
