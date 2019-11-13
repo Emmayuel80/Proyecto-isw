0e9433ee7bacae4485e9cc241f9231e4dede0db0
Silvestre <emmayuel80@gmail.com>
2019-11-13 17:40:38 -0600

    corregida funcion de crear asunto

--- services/createAsunto.js
+++ services/createAsunto.js
@@ -4,16 +4,22 @@ const pool = db.getPool();
 module.exports = function (req) {
   pool.getConnection(function (_err, connection) {
     connection.query('select idAsunto from asunto order by idAsunto DESC', function (_err, rows) {
+      const lastId = Number(rows[0].idAsunto);
       const newAsunto = {
-        IdAsunto: (Number(rows[0].idAsunto) + 1),
+        IdAsunto: (lastId + 1),
         Actividad: req.body.Actividad,
         Descripcion: req.body.Descripcion,
         FechaCreacion: new Date(),
         DiasTermino: ((parseInt(req.body.DiasTermino) === 0) ? -1 : req.body.DiasTermino),
-        RFCS: req.body.RFCS,
         Estado: 'En progreso.'
       };
       connection.query('INSERT INTO asunto SET ?', newAsunto, function (_err, _rows) {
+      });
+      const asignAsunto = {
+        IdAsunto: lastId,
+        RFCS: req.body.RFCS
+      };
+      connection.query('INSERT INTO asuntosubordinado SET ?', asignAsunto, function (_err, _rows) {
       });
     });
   });
