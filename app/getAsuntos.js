1099fa518da9ec78c47924377d2963bd33556a8c
KokiriBoy <linkhyrule025@gmail.com>
2019-11-14 22:29:46 -0600

    RFC del subordinado a funciones

--- app/getAsuntos.js
+++ app/getAsuntos.js
@@ -1,7 +1,7 @@
 const connection = require('../config/database');
 
 module.exports = function (req, callback) {
-  connection.query('select a.Actividad, a.Descripcion, a.IdAsunto from asunto a, asuntosubordinado x, subordinado s where a.IdAsunto= x.IdAsunto and x.RFCS= s.RFC and s.RFC= "' + req.RFC + '"', function (_err, _rows) {
+  connection.query('select a.Actividad, a.Descripcion, a.IdAsunto, s.RFC from asunto a, asuntosubordinado x, subordinado s where a.IdAsunto= x.IdAsunto and x.RFCS= s.RFC and s.RFC= "' + req.RFC + '"', function (_err, _rows) {
     callback(_rows);
   });
 };
