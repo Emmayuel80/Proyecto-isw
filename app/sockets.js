7adcce195ee6e747ac7c57f5fcc0fb03bd20ffa7
Silvestre <emmayuel80@gmail.com>
2019-11-15 00:42:20 -0600

    agregada funcionalidad de botones

--- app/sockets.js
+++ app/sockets.js
@@ -4,6 +4,10 @@ module.exports = function (io) {
       require('../services/changeEstado')(asunto, 1);
       io.emit('rechazado', asunto);
     });
+    socket.on('concluir', (asuntoId) => {
+      require('../services/changeEstado')(asuntoId, 5);
+      io.emit('concluido', asuntoId);
+    });
     socket.on('obtener subordinados', (idAsunto) => {
       require('./getSubordinadosAsignados')(idAsunto, (result) => {
         io.emit('subordinados asignados', result, idAsunto);
