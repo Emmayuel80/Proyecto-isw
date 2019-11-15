d00ebca4b78575d33f68bed483041c41f08d6da7
LaloCS7 <lalo.cuevas@hotmail.com>
2019-11-15 01:13:23 -0600

    rechazar asunto con las nuevas modificaciones a la bd

--- services/rechazarAsunto.js
+++ services/rechazarAsunto.js
@@ -1,9 +1,9 @@
 const db = require('../config/database');
 const pool = db.getPool();
 
-module.exports = function (dataSubject) {
+module.exports = function (dataSubject, dataSubordinado) {
   pool.getConnection(function (_err, conection) {
-    conection.query('Update asunto Set Estado="Rechazado" where idAsunto="' + dataSubject.idAsunto + '"', (_err) => {});
-    conection.query('insert into error(idAsunto, descripcion) values ("' + dataSubject.idAsunto + '"descripcion', (_error) => {});
+    conection.query('Update asunto Set Estado="Rechazado" where IdAsunto="' + dataSubject.idAsunto + '"', (_err) => {});
+    conection.query('insert into AsuntoRechazado(IdAsunto, RFCS, Descripcion) values ("' + dataSubject.idAsunto + '",' + dataSubordinado.RFCS + '",' + 'descripcion', (_error) => {});
   });
 };
