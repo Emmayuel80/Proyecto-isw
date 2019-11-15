a649677da934243e9bfd89a9147cac330ed1a225
KokiriBoy <linkhyrule025@gmail.com>
2019-11-14 23:09:28 -0600

    vista con estado del asunto

--- app/getAsuntos.js
+++ app/getAsuntos.js
@@ -1,7 +1,7 @@
 const connection = require('../config/database');
 
 module.exports = function (req, callback) {
-  connection.query('select a.Actividad, a.Descripcion, a.IdAsunto, a.Estado, a.DiasTermino, s.RFC from asunto a, asuntosubordinado x, subordinado s where a.IdAsunto= x.IdAsunto and x.RFCS= s.RFC and s.RFC= "' + req.RFC + '"', function (_err, _rows) {
+  connection.query('select a.Actividad, a.Descripcion, a.IdAsunto, a.Estado, s.RFC from asunto a, asuntosubordinado x, subordinado s where a.IdAsunto= x.IdAsunto and x.RFCS= s.RFC and s.RFC= "' + req.RFC + '"', function (_err, _rows) {
     callback(_rows);
   });
 };
