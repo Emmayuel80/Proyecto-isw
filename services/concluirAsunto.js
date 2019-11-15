d50f2592db820f9c7dd1fb9211ee45207c299af3
Silvestre <emmayuel80@gmail.com>
2019-11-15 15:57:43 -0600

    pulimiento de ui

--- services/concluirAsunto.js
+++ services/concluirAsunto.js
@@ -3,4 +3,6 @@ const connection = require('../config/database');
 module.exports = function (idAsunto) {
   connection.query('UPDATE asunto Set Estado = "Concluido." WHERE idAsunto = ' + parseInt(idAsunto) + '', function (_err, _rows) {
   });
+  connection.query('UPDATE asunto Set DiasTermino = -1 WHERE idAsunto = ' + parseInt(idAsunto) + '', function (_err, _rows) {
+  });
 };
