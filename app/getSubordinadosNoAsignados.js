3b7d562c0cd70c16ba2f8f51a3534af2a05dabc6
Silvestre <emmayuel80@gmail.com>
2019-11-21 23:12:02 -0600

    arreglado bug al obtener subordinados

--- app/getSubordinadosNoAsignados.js
+++ app/getSubordinadosNoAsignados.js
@@ -1,7 +1,7 @@
 const connection = require('../config/database');
 
 module.exports = function (idAsunto, rfc, callback) {
-  connection.query(' Select DISTINCT s.* from subordinado s, asunto a, asuntosubordinado x, encargado e where e.rfc=s.rfce and s.rfc!=x.rfcs and x.idasunto=a.idasunto and a.idasunto=' + idAsunto + ' and e.rfc="' + rfc + '"', function (_err, _rows) {
+  connection.query('select s.* from subordinado s where not EXISTS( SELECT a.IdAsunto from asuntosubordinado a WHERE a.RFCS=s.RFC and a.IdAsunto=' + idAsunto + ') and s.RFCE="' + rfc + '"', function (_err, _rows) {
     callback(_rows);
   });
 };
