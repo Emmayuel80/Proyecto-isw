761cb5427ef0942359e8f535cff3b8c8db4c89e9
Silvestre <emmayuel80@gmail.com>
2019-11-21 22:36:23 -0600

    registro de subordinados

--- config/register.js
+++ config/register.js
@@ -7,7 +7,7 @@ const saltRounds = 10;
 // =========================================================================
 // we are using named strategies since we have one for login and one for signup
 // by default, if there was no name, it would just be called 'local'
-module.exports = function (req) {
+module.exports = function (req, RFCE) {
   connection.query("select * from subordinado where email = '" + req.body.email + "'", function (err, rows) {
     console.log(rows);
     console.log('above row object');
@@ -15,26 +15,21 @@ module.exports = function (req) {
     if (rows.length) {
       return req.flash('signupMessage', 'Esa dirección de correo electronico ya existe.');
     } else {
-      var encrypt;
       bcrypt.hash(req.body.RFC, saltRounds, function (_err, hash) {
-        encrypt = hash;
-      });
-      // if there is no user with that email
-      // create the user
-      var newUserMysql = {
-        RFC: req.body.RFC,
-        Nombre: req.body.name,
-        ApPaterno: req.body.apPaterno,
-        ApMaterno: req.body.apMaterno,
-        colonia: req.body.colonia,
-        tel: parseInt(req.body.tel),
-        Email: req.body.email,
-        Contrasena: encrypt // use the generateHash function in our user model
-      };
-      var insertQuery = 'INSERT INTO subordinado SET ?';
-      console.log(insertQuery);
-      connection.query(insertQuery, newUserMysql, function (_err, _rows) {
-        return true;
+        // if there is no user with that email
+        // create the user
+        var newUserMysql = {
+          RFC: req.body.RFC,
+          Nombre: req.body.Nombre,
+          ApPaterno: req.body.ApPaterno,
+          ApMaterno: req.body.ApMaterno,
+          Email: req.body.email,
+          Contrasena: hash,
+          RFCE: RFCE // use the generateHash function in our user model
+        };
+        console.log(newUserMysql);
+        connection.query('INSERT INTO subordinado SET ?', newUserMysql, function (_err, _rows) {
+        });
       });
     }
   });
